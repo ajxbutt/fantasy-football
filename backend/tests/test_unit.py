8893388864ade26edb3f1a97456aa1a537f404dd
Ubuntu <aj@kiwi.1o5aawszdpmu1cb12ui31cweea.zx.internal.cloudapp.net>
2021-12-02 13:59:46 +0000

    added more lines to testing for better coverage

--- backend/tests/test_unit.py
+++ backend/tests/test_unit.py
@@ -53,6 +53,15 @@ class TestCreate(TestBase):
         )
         self.assertEquals(b"Added team with name: team a", response.data)
         self.assertEquals(Teams.query.get(1).name, "team a")
+
+    def test_create_player(self, team_id):
+        response = self.client.post(
+            url_for('create_player'),
+            json={"name": "team a", "position": "ST"},
+            follow_redirects=True
+        )
+        self.assertEquals(b"Added team with name: team a", response.data)
+        self.assertEquals(Teams.query.get(1).name, "team a")
     
 class TestUpdate(TestBase):
 
@@ -72,10 +81,22 @@ class TestUpdate(TestBase):
         self.assertEquals(b"Updated team (ID: 1) with league: league a", response.data)
         self.assertEquals(Teams.query.get(1).league, "league a")
         
+    def test_update_player_name(self):
+        response = self.client.put(
+            url_for('update_player_name', id=1),
+            json={"name": "team a"}
+        )
+        self.assertEquals(b"Updated player (ID: 1) with name: team a", response.data)
+        self.assertEquals(Teams.query.get(1).league, "league a")
 
 class TestDelete(TestBase):
 
     def test_delete_team(self):
         response = self.client.delete(url_for('delete_team', id=1))
         self.assertEquals(b"Deleted team (ID: 1)", response.data)
+        self.assertIsNone(Teams.query.get(1))
+
+    def test_delete_player(self):
+        response = self.client.delete(url_for('delete_player', id=1))
+        self.assertEquals(b"Deleted player (ID: 1)", response.data)
         self.assertIsNone(Teams.query.get(1))
